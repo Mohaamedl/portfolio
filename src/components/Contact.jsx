7efcee5414be78497aa3884374fb3085da173945
mohamed <94742483+Mohaamedl@users.noreply.github.com>
2024-06-30 16:49:26 +0100

    fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -92,8 +92,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        env.VITE_APP_EMAILJS_SERVICE_ID,
+        env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
           to_name: "Mohamed",
@@ -101,7 +101,7 @@ const Contact = () => {
           to_email: "mohamedhadodo33@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        env.VITE_APP_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
