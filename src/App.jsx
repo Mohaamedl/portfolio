4e28f34546c86849d74a9c040625f21bc0749242
mohamed <94742483+Mohaamedl@users.noreply.github.com>
2024-06-30 16:34:43 +0100

    speedInsights added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import React from 'react';
-import { StarsCanvas } from './components/canvas';
 import { BrowserRouter } from "react-router-dom";
 import { About, Contact, Experience, Feedback, Home, Navbar, ParticlesContainer, Tech, Works } from './components';
+import { StarsCanvas } from './components/canvas';
 const App = () => {
   return(
   <>
-  
+  <SpeedInsights/>
   <BrowserRouter>
     <div className = "relative z-0">
     <ParticlesContainer></ParticlesContainer>
